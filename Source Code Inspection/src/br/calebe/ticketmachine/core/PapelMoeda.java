425a29895ceefd453a6197fe1aa95b1636984490
Letícia Santiago da Silva <101338974+LehSantiago@users.noreply.github.com>
2025-08-29 19:33:42 -0300

    Update PapelMoeda.java
    Adicionando os defeitos

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -1,24 +1,27 @@
 package br.calebe.ticketmachine.core;
-
 /**
- *
- * @author Calebe de Paula Bianchini
- */
+* Corretoras:
+* Isadora Caetano Brandão de Sousa - 10420646
+* Letícia Santiago da Silva - 10420655
+*
+* @author Calebe de Paula Bianchini
+*/
 public class PapelMoeda {
-
-    protected int valor;
-    protected int quantidade;
-
-    public PapelMoeda(int valor, int quantidade) {
-        this.valor = valor;
-        this.quantidade = quantidade;
-    }
-
-    public int getValor() {
-        return valor;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
+   protected int valor;
+   protected int quantidade;
+   /*
+	 * Tipo de Defeito: Comissão
+	 *
+	 * Não há validação para verificar se os valores são negativos.
+	 * */
+   public PapelMoeda(int valor, int quantidade) {
+       this.valor = valor;
+       this.quantidade = quantidade;
+   }
+   public int getValor() {
+       return valor;
+   }
+   public int getQuantidade() {
+       return quantidade;
+   }
 }
